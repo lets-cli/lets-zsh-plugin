69c7e7b0d1e5b0d00b255f48cc380dc11b0954da
Kindritskiy Max <kindritskiy.m@gmail.com>
2022-07-12 10:32:44 +0300

    improve plugin loading

--- lets.plugin.zsh
+++ lets.plugin.zsh
@@ -1,1 +1,16 @@
 # Autocompletion for lets.
+
+# According to the standard:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Load the plugin itself to fpath
+if [[ ${zsh_loaded_plugins[-1]} != */lets && -z ${fpath[(r)${0:h}]} ]]
+then
+  fpath+=( "${0:h}" )
+fi
+
+if [[ ! -f _lets ]]; then
+    lets completion -s zsh --verbose > _lets
+fi
